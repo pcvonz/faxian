4703ea43e83055387af3eb158074217926e658d9
Paull Von Zimmerman <pcvonz@gmail.com>
2017-03-13 21:41:14 -0700

    sustainability

--- source/js/init_fullpage.js
+++ source/js/init_fullpage.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     { 
       anchors:['paint_nav', 'roof_nav', 'wall_nav', 'floor_nav', 'window_nav'],
       scrollOverflow: true,
-      responsiveHeight: 0,
+      responsiveHeight: 800,
       responsiveWidth: 768
     });
 });
